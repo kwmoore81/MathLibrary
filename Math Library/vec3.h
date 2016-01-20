c9caea1a89c941180c4c3a9a38a5b31072e2821e
Kevin Moore <kwmoore81@gmail.com>
2016-01-20 12:42:20 -0800

    Added functions

--- Math Library/vec3.h
+++ Math Library/vec3.h
@@ -1,111 +1,113 @@
 #pragma once
+#pragma once
 #include <cfloat>
 #include <cmath>
 //FLT_EPSILON for comparison operators
 //float a, b;
 //a == b;
-	// (a - FLT_EPSILON) < b && b < (a + FLT_EPSILON)
-	//fabs(a-b) < FLT_EPSILON
+// (a - FLT_EPSILON) < b && b < (a + FLT_EPSILON)
+//fabs(a-b) < FLT_EPSILON
 
 #define PI 3.14159265359
 
 namespace kml
 {
-	struct vec2
+	struct vec3
 	{
 		union
 		{
-			struct { float x, y; };
-			float v[2];
+			struct { float x, y, z; };
+			float v[3];
 		};
 		//static construction functions
-		static vec2 fromAngle(float angle);
+		static vec3 fromAngle(float angle);
 		//unary oerations
 		float magnitude() const; //pythagorean theorm
 		float angle() const; //atan2f
-		vec2 normal() const; //divide by magnitude
-		vec2 perp() const; //<-y,x>
+		vec3 normal() const; //divide by magnitude
+		vec3 perp() const; //<-y,x>
 		void normalize();
-		vec2 operator-() const; //<-x,-y>
-		vec2 Reflection() const;
-		
+		vec3 operator-() const; //<-x,-y>
+		vec3 Reflection() const;
+
 	};
 	//binary operators
-	float dot(const vec2 &lhs, const vec2 &rhs);
-	vec2 operator+(const vec2 &lhs, const vec2 &rhs);
-	vec2 operator+=(const vec2 &lhs, const vec2 &rhs);
-	vec2 operator-(const vec2 &lhs, const vec2 &rhs);
-	vec2 operator-=(const vec2 &lhs, const vec2 &rhs);
+	float dot(const vec3 &lhs, const vec3 &rhs);
+	vec3 operator+(const vec3 &lhs, const vec3 &rhs);
+	vec3 operator+=(const vec3 &lhs, const vec3 &rhs);
+	vec3 operator-(const vec3 &lhs, const vec3 &rhs);
+	vec3 operator-=(const vec3 &lhs, const vec3 &rhs);
 
-	vec2 operator*(const vec2 &lhs, float rhs);
-	vec2 operator*=(const vec2 &lhs, float rhs);
-	vec2 operator/(const vec2 &lhs, float rhs);
-	vec2 operator/=(const vec2 &lhs, float rhs);
+	vec3 operator*(const vec3 &lhs, float rhs);
+	vec3 operator*=(const vec3 &lhs, float rhs);
+	vec3 operator/(const vec3 &lhs, float rhs);
+	vec3 operator/=(const vec3 &lhs, float rhs);
 
-	bool operator==(const vec2 &lhs, const vec2 &rhs);
-	bool operator!=(const vec2 &lhs, const vec2 &rhs);
-	bool operator<(const vec2 &lhs, const vec2 &rhs);
-	bool operator<=(const vec2 &lhs, const vec2 &rhs);
-	bool operator>(const vec2 &lhs, const vec2 &rhs);
-	bool operator>=(const vec2 &lhs, const vec2 &rhs);
-	
+	bool operator==(const vec3 &lhs, const vec3 &rhs);
+	bool operator!=(const vec3 &lhs, const vec3 &rhs);
+	bool operator<(const vec3 &lhs, const vec3 &rhs);
+	bool operator<=(const vec3 &lhs, const vec3 &rhs);
+	bool operator>(const vec3 &lhs, const vec3 &rhs);
+	bool operator>=(const vec3 &lhs, const vec3 &rhs);
+
 }
 
 using namespace kml;
 
-vec2 vec2::fromAngle(float angle)
-{
-	vec2 angVec2;
-	angle = angle * PI / 180;
+//vec3 vec3::fromAngle(float angle)
+//{
+//	vec3 angVec2;
+//	angle = angle * PI / 180;
+//
+//	angVec2.x = cos(angle);
+//	angVec2.y = sin(angle);
+//
+//	return angVec2;
+//}
 
-	angVec2.x = cos(angle);
-	angVec2.y = sin(angle);
-	
-	return angVec2;
+float vec3::magnitude() const
+{
+	return sqrt(x*x + y*y + z*z);
 }
 
-float vec2::magnitude() const
+float vec3::angle() const
 {
-	return sqrt(x*x + y*y);
+	return atan2f(y, x, z) * 180 / PI;
 }
 
-float vec2::angle() const
-{
-	return atan2f(y, x) * 180 / PI;
-}
-
-inline vec2 kml::vec2::Reflection() const
+inline vec3 kml::vec3::Reflection() const
 {
 	//check on this
-	-vec2::magnitude();
-	-vec2::angle();
-	return vec2();
+	-vec3::magnitude();
+	-vec3::angle();
+	return vec3();
 }
 
-vec2 vec2::normal() const
+vec3 vec3::normal() const
 {
-	vec2 normVec2;
-	  normVec2.x = x / vec2::magnitude();
-	  normVec2.y = y / vec2::magnitude();
+	vec3 normVec3;
+	normVec3.x = x / vec3::magnitude();
+	normVec3.y = y / vec3::magnitude();
+	normVec3.z = z / vec3::magnitude();
 
-	return normVec2;
+	return normVec3;
 }
 
-inline vec2 vec2::perp() const
+inline vec3 vec3::perp() const
 {
-	vec2 perpVec2;
+	vec3 perpVec3;
 
-	perpVec2.x = -y;
-	perpVec2.y = x;
-	
-	return perpVec2;
+	perpVec3.x = -y;
+	perpVec3.y = x;
+
+	return perpVec3;
 }
 
-inline void vec2::normalize()
+inline void vec3::normalize()
 {
-	x = x / vec2::magnitude();
-	y = y / vec2::magnitude();
-
+	x = x / vec3::magnitude();
+	y = y / vec3::magnitude();
+	z = z / vec3::magnitude();
 }
 
 inline vec2 vec2::operator-() const
@@ -126,7 +128,7 @@ float kml::dot(const vec2 & lhs, const vec2 & rhs)
 vec2 kml::operator+(const vec2 & lhs, const vec2 & rhs)
 {
 	vec2 plusVec2;
-		
+
 	plusVec2.x = lhs.x + rhs.x;
 	plusVec2.y = lhs.y + rhs.y;
 
@@ -136,7 +138,7 @@ vec2 kml::operator+(const vec2 & lhs, const vec2 & rhs)
 vec2 kml::operator+=(const vec2 & lhs, const vec2 & rhs)
 {
 	vec2 plEqVec2;
-	
+
 	plEqVec2.x = lhs.x + rhs.x;
 	plEqVec2.y = lhs.y + rhs.y;
 
@@ -206,7 +208,7 @@ vec2 kml::operator/=(const vec2 & lhs, float rhs)
 bool kml::operator==(const vec2 & lhs, const vec2 & rhs)
 {
 	if (fabs(lhs.x - rhs.x < FLT_EPSILON) && fabs(lhs.y - rhs.y < FLT_EPSILON)) { return true; }
-	
+
 	else { return false; }
 }
 
